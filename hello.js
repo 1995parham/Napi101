ace51908a227dcec3a00d8226131ca7c5deb48c6
Parham Alvani <parham.alvani@gmail.com>
2017-06-08 23:19:30 +0430

    Hello.js uses the hello c napi addon.

---
+++ hello.js
@@ -0,0 +1,12 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani (parham.alvani@gmail.com)
+ * |
+ * | Creation Date: 08-06-2017
+ * |
+ * | File Name:     hello.js
+ * +===============================================
+ */
+const hello = require('./build/Release/hello');
+
+console.log(hello.hello());
